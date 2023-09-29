c9cf92816f36f1b50929e134d021b0489d8f5934
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2023-09-29 13:27:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import discord
-from discord.ext import commands
-import dotenv
-import os
+def main(bot, token):
+    @bot.event
+    async def on_ready():
+        await bot.load_extension("jishaku")
 
-dotenv.load_dotenv()
-
-bot = commands.Bot(intents=discord.Intents.all(), command_prefix="tca!")
-
-@bot.event
-async def on_ready():
-    await bot.load_extension("jishaku")
-
-bot.run(token=os.getenv("TCABOT_TOKEN"))
+    bot.run(token=token)
