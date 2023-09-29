07c43079f3777a33a36ea0463dd34117ee705ec4
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2023-09-29 13:32:07 +0900

    Update run.py

--- run.py
+++ run.py
@@ -9,5 +9,13 @@ dotenv.load_dotenv()
 
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix="tca!")
 
+@bot.command()
+@bot.is_owner()
+async def restart(ctx):
+    await ctx.send("Restart now...")
+    await bot.close()
+    main = importlib.reload_module("main")
+    main.main(bot, token=os.getenv("TCABOT_TOKEN"))
+
 main = importlib.import_module("main")
 main.main(bot, token=os.getenv("TCABOT_TOKEN"))
