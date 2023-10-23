7722c254d5137b6e887c799869e5a69a42e667f6
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2023-10-24 08:04:49 +0900

    change how to run

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import traceback
+import discord
+from discord.ext import commands
 import os
+import dotenv
 
-def main(bot, token):
-    @bot.event
-    async def on_ready():
-        await bot.load_extension("jishaku")
-        for name in os.listdir("cogs"):
-            if not name.startswith("."):
-                try:
-                    await bot.load_extension("cogs."+name.replace(".py", ""))
-                except Exception as e:
-                    print("".join(traceback.format_exception(e)))
-    bot.run(token=token)
+
+dotenv.load_dotenv()
+
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix="tca!")
+
+@bot.event
+async def on_ready():
+    await bot.load_extension("jishaku")
+    for name in os.listdir("cogs"):
+        if not name.startswith((".", "_")):
+            try:
+                await bot.load_extension("cogs."+name.replace(".py", ""))
+            except Exception as e:
+                print("".join(traceback.format_exception(e)))
+
+
+bot.run(token=os.getenv("TCABOT_TOKEN"))
--- run.py
+++ run.py
@@ -9,12 +9,5 @@ dotenv.load_dotenv()
 
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix="tca!")
 
-@bot.command()
-@commands.is_owner()
-async def restart(ctx):
-    await ctx.send("Restart now...")
-    main = importlib.reload_module("main")
-    main.main(bot, token=os.getenv("TCABOT_TOKEN"))
-
 main = importlib.import_module("main")
 main.main(bot, token=os.getenv("TCABOT_TOKEN"))
