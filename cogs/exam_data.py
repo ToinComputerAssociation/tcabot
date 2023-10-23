ac9deb946c7761b541163801df70567f939a683d
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2023-10-24 08:41:39 +0900

    Create exam_data.py

---
+++ cogs/exam_data.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+import discord
+import aiomysql
+
+class Examination(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    async def load_cog(self):
+        #self.pool = await aiomysql.create_pool(
+        #    host=os.environ["MYSQL_HOST"], port=int(os.environ["MYSQL_PORT"]),
+        #    user=os.environ["MYSQL_USERNAME"], password=os.environ["MYSQL_PASSWORD"],
+        #    db=os.environ["MYSQL_DBNAME"], loop=self.loop, autocommit=True
+        #)
+        pass
+
+    @commands.command(aliases=["re"])
+    async def register(self, ctx: commands.Context):
+        await ctx.send("test!")
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Examination(bot))
