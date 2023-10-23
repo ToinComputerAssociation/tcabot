3ea3e312e31bdb52a12cd7d954cacdcddcc9d4b6
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2023-10-24 08:20:48 +0900

    cwd update

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from discord.ext import commands
 import os
 import dotenv
 
+# cwdをこのファイルがある場所に移動
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 
 dotenv.load_dotenv()
 
@@ -12,12 +15,13 @@ bot = commands.Bot(intents=discord.Intents.all(), command_prefix="tca!")
 @bot.event
 async def on_ready():
     await bot.load_extension("jishaku")
-    for name in os.listdir("cogs"):
+    for name in os.listdir("./cogs"):
         if not name.startswith((".", "_")):
             try:
                 await bot.load_extension("cogs."+name.replace(".py", ""))
             except Exception as e:
                 print("".join(traceback.format_exception(e)))
+    print("Hello, TCABot!")
 
 
 bot.run(token=os.getenv("TCABOT_TOKEN"))
