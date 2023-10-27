9d9e1e9fa8685583b41539da3d00db0280dfcf2d
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2023-10-28 08:05:09 +0900

    Update debug.py

--- cogs/debug.py
+++ cogs/debug.py
@@ -4,5 +4,10 @@ class Debug(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.hybrid_command(aliases=["sql"])
+    async def execute_tsuq_sql(self, ctx, arg):
+        r = await self.bot.cogs["Examination"].execute_sql(arg)
+        await ctx.send(f"Command was executed successfully.\n```\n{r or '(Nothing Returned)'}\n```")
+
 async def setup(bot):
     await bot.add_cog(Debug(bot))
