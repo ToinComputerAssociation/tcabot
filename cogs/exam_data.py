7af530fbaf22b9b9f7dcffb9a50c8596b99ffff6
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2023-10-31 13:54:00 +0900

    Update exam_data.py

--- cogs/exam_data.py
+++ cogs/exam_data.py
@@ -4,43 +4,65 @@ import aiomysql
 import os
 
 
+class DataCreateView(discord.ui.View):
+    def get_item(self, cls):
+        for i in self.children:
+            if isinstance(i, cls):
+                return i
+        raise KeyError("The class %r not found." % cls)
+
+
 class TestType(discord.ui.Select):
-    def __init__(self, default=None):
+    def __init__(self):
         super().__init__(
             placeholder="テスト種別を選択...",
             options=[
-                discord.SelectOption(label="前期中間", value="1", default=default==1),
-                discord.SelectOption(label="前期期末", value="2", default=default==2),
-                discord.SelectOption(label="後期中間", value="3", default=default==3),
-                discord.SelectOption(label="後期期末", value="4", default=default==4)
+                discord.SelectOption(label="前期中間", value="1"),
+                discord.SelectOption(label="前期期末", value="2"),
+                discord.SelectOption(label="後期中間", value="3"),
+                discord.SelectOption(label="後期期末", value="4")
             ]
         )
 
     async def callback(self, interaction):
         selected = int(self.values[0])
-        # self.view.add_item()
-        await interaction.response.edit_message(
-            content=f"selected {selected}", view=self.view
-        )
+        for i in range(4):
+            self.options[i].default = (i == selected-1)
+        await interaction.response.edit_message(view=self.view)
 
 
 class Grade(discord.ui.Select):
-    def __init__(self, default=None):
+    def __init__(self):
         super().__init__(
             placeholder="学年を選択...",
             options=[
-                discord.SelectOption(label="1年生", value="1", default=default==1),
-                discord.SelectOption(label="2年生", value="2", default=default==2),
-                discord.SelectOption(label="3年生", value="3", default=default==3)
+                discord.SelectOption(label="1年生", value="1"),
+                discord.SelectOption(label="2年生", value="2"),
+                discord.SelectOption(label="3年生", value="3")
             ]
         )
 
     async def callback(self, interaction):
         selected = int(self.values[0])
-        # self.view.add_item()
-        await interaction.response.edit_message(
-            content=f"selected {selected}", view=self.view
-        )
+        for i in range(4):
+            self.options[i].default = (i == selected-1)
+        await interaction.response.edit_message(view=self.view)
+
+
+class SubmitButton(discord.ui.Button):
+    def __init__(self, mode: int):
+        super().__init__(label="次へ")
+        self.mode = mode
+
+    async def callback(self, interaction):
+        if self.mode == 1:
+            # self.view.add_item()
+            grade = self.view.get_item(Grade)
+            if not grade.values:
+                return await interaction.response.send_message("先に学年を指定してください。", ephemeral=True)
+            await interaction.response.edit_message(content="教科を選択してください。")
+        else:
+            ...
 
 
 class Examination(commands.Cog):
@@ -68,10 +90,11 @@ class Examination(commands.Cog):
 
     @commands.hybrid_command(aliases=["re"])
     async def register(self, ctx: commands.Context):
-        view = discord.ui.View()
+        view = DataCreateView()
         view.add_item(Grade())
         view.add_item(TestType())
-        await ctx.send("test!", view=view)
+        view.add_item(SubmitButton(1))
+        await ctx.send("学年とテストの種類を選択してください。", view=view)
 
     @commands.hybrid_command()
     async def view(self, ctx: commands.Context):
